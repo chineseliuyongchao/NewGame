3b25201502cfb3c1fa9a2cef1cb83f05a84767a3
LiuYongchao <1215266098@qq.com>
2023-12-10 20:54:47 +0800

    完善项目文档，添加聚落生成

--- Assets/Res/File/Excel2Json2Cs/JsonUsingStandards.md
+++ Assets/Res/File/Excel2Json2Cs/JsonUsingStandards.md
@@ -1,2 +1,4 @@
 ﻿# json使用规范
 数据表必须有ID，Name两条属性，表的名字是Sheet1
+# 数据表对应信息
+SettlementInformation 存放聚落信息
--- Assets/Scripts/Game/Town/TownNode.cs
+++ Assets/Scripts/Game/Town/TownNode.cs
@@ -1,4 +1,5 @@
-﻿using GameQFramework;
+﻿using System.Collections.Generic;
+using GameQFramework;
 using QFramework;
 using UnityEngine;
 
@@ -10,11 +11,31 @@ namespace Game.Town
     public class TownNode : BaseGameController
     {
         public TextAsset textAsset;
+        public BaseTown baseTown;
+        private Dictionary<string, BaseTown> _towns;
 
         protected override void OnInit()
         {
             base.OnInit();
             this.GetSystem<ITownSystem>().InitTownData(textAsset);
+            _towns = new Dictionary<string, BaseTown>();
+            InitTown();
+        }
+
+        /// <summary>
+        /// 生成所有城市
+        /// </summary>
+        private void InitTown()
+        {
+            Dictionary<string, Town> townData = this.GetModel<ITownModel>().TownData;
+            foreach (var kvp in townData)
+            {
+                string key = kvp.Key; // 获取键
+                Town value = kvp.Value; // 获取值
+                BaseTown town = Instantiate(baseTown, transform);
+                town.gameObject.transform.position = new Vector3(value.TownPos[0], value.TownPos[1], 0);
+                _towns.Add(key, town);
+            }
         }
     }
 }
